7477cbdce6991e4ab4b50d1c4abf39ba48ba551a
goecmenru91738 <goecmenru91738@th-nuernberg.de>
2024-02-21 15:25:50 +0100

    final version

--- script.js
+++ script.js
@@ -7,16 +7,22 @@ const computerPaddle = new Paddle(document.getElementById("computer-paddle"));
 const playerScoreElem = document.getElementById("player-score");
 const computerScoreElem = document.getElementById("computer-score");
 
-
+//Schleife/Rekursion, um Spiel zu starten
 let lastTime;
 function update(time) {
 
     if (lastTime != null) {
         const delta = time - lastTime;
 
+        //Ball/ComputerPaddle Position ändern
         ball.update(delta, [playerPaddle.rect(), computerPaddle.rect()]);
         computerPaddle.update(delta, ball.y);
 
+        //Hintergrund ändern
+        const hue = parseFloat(getComputedStyle(document.documentElement).getPropertyValue("--hue"));
+        document.documentElement.style.setProperty("--hue", hue + 0.01 * delta);
+
+        //Abfrage für einen Lose Fall
         if (isLose()) handleLose();
     }
 
@@ -24,11 +30,13 @@ function update(time) {
     window.requestAnimationFrame(update);
 }
 
+//Hat einer verloren?
 function isLose() {
     const rect = ball.rect();
     return rect.right >= window.innerWidth || rect.left <= 0;
 }
 
+//Händle das Verlieren
 function handleLose() {
     const rect = ball.rect();
     if (rect.right >= window.innerWidth) {
@@ -41,8 +49,10 @@ function handleLose() {
     computerPaddle.reset();
 }
 
+//SpielerPaddle folgt der Maus
 document.addEventListener("mousemove", e => {
     playerPaddle.position = (e.y / window.innerHeight) * 100;
 })
 
+//Spiel Start
 window.requestAnimationFrame(update);
